e49f8f498a8d9215610b84bbceec2a50bad6ca5d
Yovany Cunha <yovany33@gmail.com>
2019-04-05 09:13:31 -0300

    scraping url

--- ri_lab_01/spiders/carta_capital.py
+++ ri_lab_01/spiders/carta_capital.py
@@ -18,14 +18,25 @@ class CartaCapitalSpider(scrapy.Spider):
         self.start_urls = list(data.values())
 
     def parse(self, response):
-        #
-        # inclua seu código aqui
-        #
+
+        for href in response.css("div.eltdf-wrapper-inner a::attr(href)"):
+            print(href)
+            yield response.follow(href, self.scrap_url, meta={'url', response.url}
+
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
-        #
-        #
-        #
+    
+
+    def scrap_url(self, response):
+        yield {
+            'categoria' : response.css("div.eltdf-post-info-category > a::text").get(default='').strip(),
+            'titulo': response.css("a.eltdf-pt-link::text").get(default='').strip(),
+            'subtitulo': response.css("div.wpb_wrapper > h3::text").get(default='').strip(),
+            'url': response.url,
+            'autor': response.css("a.eltdf-post-info-author-link::text").get(default='').strip(),
+            'data': response.css("div.eltdf-post-info-date > a::text").get(default='').strip(),
+            'texto': response.css("div.eltdf-post-text-inner > a::text, div.eltdf-post-text-inner > p::text").getall()
+        }
